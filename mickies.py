17b456f5b184c56c71c79c083564865dcfcd3690
Ciccolo22 <66913102+Ciccolo22@users.noreply.github.com>
2021-02-20 17:20:42 -0800

    Add files via upload

---
+++ mickies.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import pandas as pd
+import time
+data = pd.read_csv('micky.csv', index_col='Item')
+
+st.sidebar.header("Micky's D's Breakfast Item Comparison App")
+
+name = st.sidebar.text_input('Name Please')
+
+if name:
+    st.sidebar.write("Be careful eating too much McDonald's", name)
+    time.sleep(1)
+    st.sidebar.markdown("*It's later than you think...*")
+
+
+st.sidebar.subheader("menu item")
+
+item_selection = st.sidebar.selectbox(
+"Please select an item",
+("","Egg Mcmuffin", "Big Mac", "Large fry"))
+
+
+metric_selection = st.sidebar.selectbox(
+"Please select a metric",
+("","Total Fat", "Cholesterol", "Sodium"))
+
+
+showframe = st.checkbox('Show example data')
+
+if showframe:
+    st.table(data)
+    
+    
+   
+  
+
+if metric_selection =="Total Fat":
+    st.write("Product Comparison (fat content)")
+    st.bar_chart(data.iloc[0:3, 0:1], width=1.3)              
+       
+
+if metric_selection =="Cholesterol":
+    st.write("Product Comparison (fat content)")
+    st.bar_chart(data.iloc[0:3, 1:2], width=1.3)      
+
+
+  
+if metric_selection =="Sodium":
+    st.write("Product Comparison (fat content)")
+    st.bar_chart(data.iloc[0:3, 2:], width=1.3)            
