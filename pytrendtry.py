d3a3c77948379c495acab839aa6639f17a38a2b4
Ciccolo22 <66913102+Ciccolo22@users.noreply.github.com>
2021-05-29 04:24:24 -0700

    Add files via upload

---
+++ pytrendtry.py
@@ -0,0 +1,35 @@
+
+##pytrends
+
+
+
+import streamlit as st
+import pandas as pd
+from pytrends.request import TrendReq
+import matplotlib
+import time
+
+@st.cache
+def trend():
+    pytrend = TrendReq()
+    df = pytrend.trending_searches(pn='united_states')
+    df.columns=['topic']
+    return df
+
+
+checkbox = st.checkbox('Click me to find out what is trending!!')
+
+
+if checkbox:
+  with st.spinner('Wait for it...'):
+    time.sleep(3)
+    st.success('Done!')
+  st.table(trend())
+    
+
+
+st.sidebar.header(""" 
+Trends Prototype volume 1""") 
+
+
+st.sidebar.write("Need somewhere to find out what is trending in a short amount of time? Try us out")
